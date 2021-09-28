eb57e63f9160e7da451db353e9efbf6d7dc94d76
brmurrell3 <brmurrell3@gmail.com>
2021-09-28 12:56:58 -0400

    Fixed problem 5 bug

--- Problem5.c
+++ Problem5.c
@@ -22,9 +22,9 @@ int lcm(int lcmNum, int lastMod) {
     }
 }
 
-char isDiv(int a) {
-    // Loop bypasses checks for mod 2, 4, 5, 10
-    // Other mod operators bypass checks for 3 (12, 18), 6 (12, 18), 7 (14), 8 (16), 9 (18)
+char isDiv(long a) {
+    // Loop bypasses checks for mod 2, 4, 5, 10 and 20 by incrimenting by 20
+    // Other mod operators bypass checks for 3 (12, 15, 18), 6 (12, 18), 7 (14), 8 (16), 9 (18)
     // Compiler optimizes for const mod divisor
     if ((a % 19 != 0) || (a % 18 != 0) || (a % 17 != 0) || (a % 16 != 0) || (a % 15 != 0) ||
         (a % 14 != 0) || (a % 13 != 0) || (a % 12 != 0) || (a % 11 != 0)) {
@@ -37,7 +37,7 @@ int main (int argc, char** argv) {
     int smallestNum = 20;
     
     while (1) {
-        if (isDiv(smallestNum)) {
+        if (isDiv(smallestNum) == 1) {
             break;
         }
         smallestNum += 20;
